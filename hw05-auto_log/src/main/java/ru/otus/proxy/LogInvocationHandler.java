721831a6654678abd44b382f510b347f939cde65
Alex <bugrimov_a@bk.ru>
2020-08-31 22:37:47 +0300

    [#11] Speed fix

--- hw05-auto_log/src/main/java/ru/otus/proxy/LogInvocationHandler.java
+++ hw05-auto_log/src/main/java/ru/otus/proxy/LogInvocationHandler.java
@@ -3,27 +3,47 @@ package ru.otus.proxy;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.List;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class LogInvocationHandler implements InvocationHandler {
 
     private final Object target;
+    private final List<Method> logMethods;
 
     LogInvocationHandler(Object target) {
         this.target = target;
+        this.logMethods = getMethodsBy(target, (method) -> method.isAnnotationPresent(Log.class));
     }
 
     @Override
     public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-
-        if (LoggingAnnotation.isPresentAnnotation(target, method, Log.class)) {
-            LoggingAnnotation.log(
-                    "executed method: %s, params: %s%n",
-                    method.getName(), Arrays.stream(args)
-                            .map(Object::toString)
-                            .collect(Collectors.joining(", "))
-            );
-        }
+        logInfo(method, args);
         return method.invoke(target, args);
+    }
+
+    private void logInfo(Method method, Object[] args) {
+        final String methodName = method.getName();
+        final Class<?>[] parameterTypes = method.getParameterTypes();
+        for (Method logMethod : logMethods) {
+            if (methodName.equals(logMethod.getName())
+                    && Arrays.equals(parameterTypes, logMethod.getParameterTypes())) {
+                System.out.printf("executed method: %s, params: %s%n", logMethod.getName(), argsAsString(args));
+                return;
+            }
+        }
+    }
+
+    private List<Method> getMethodsBy(Object target, Predicate<Method> methodPredicate) {
+        return Arrays.stream(target.getClass().getMethods())
+                .filter(methodPredicate)
+                .collect(Collectors.toList());
+    }
+
+    private String argsAsString(Object[] args) {
+        return Arrays.stream(args)
+                .map(String::valueOf)
+                .collect(Collectors.joining(", "));
     }
 }
