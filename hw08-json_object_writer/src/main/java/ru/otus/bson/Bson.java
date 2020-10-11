cc4caea3f939b1973c86c1212e55f06053f2d0cf
Alex <bugrimov_a@bk.ru>
2020-10-11 16:12:26 +0300

    [#13] Fix tests

--- hw08-json_object_writer/src/main/java/ru/otus/bson/Bson.java
+++ hw08-json_object_writer/src/main/java/ru/otus/bson/Bson.java
@@ -2,13 +2,9 @@ package ru.otus.bson;
 
 import ru.otus.bson.handlers.ObjectHandler;
 
-import static ru.otus.bson.handlers.Predicates.isNull;
-
 public class Bson {
 
     public String toJson(Object object) {
-        return isNull.test(object)
-                ? "{}" :
-                ObjectHandler.handle(object).toString();
+        return ObjectHandler.handle(object).toString();
     }
 }
--- hw08-json_object_writer/src/test/java/ru/otus/bson/BsonTest.java
+++ hw08-json_object_writer/src/test/java/ru/otus/bson/BsonTest.java
@@ -1,10 +1,15 @@
 package ru.otus.bson;
 
+import com.google.gson.Gson;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.otus.entities.Avatar;
 
+import java.util.Collections;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BsonTest {
 
@@ -21,7 +26,7 @@ public class BsonTest {
     public void successfulSerializationNullObject() {
         final Avatar avatar = null;
         final String json = new Bson().toJson(avatar);
-        assertThat(json).isEqualTo("{}");
+        assertThat(json).isEqualTo("null");
     }
 
     @Test
@@ -30,5 +35,23 @@ public class BsonTest {
         final int[] intArray = {1, 2, 3, 4, 5};
         final String json = new Bson().toJson(intArray);
         assertThat(json).isEqualTo("[1,2,3,4,5]");
+    }
+
+    @Test
+    void test() {
+        var gson = new Gson();
+        var serializer = new Bson();
+        assertEquals(gson.toJson(null), serializer.toJson(null));
+        assertEquals(gson.toJson((byte)1), serializer.toJson((byte)1));
+        assertEquals(gson.toJson((short)1f), serializer.toJson((short)1f));
+        assertEquals(gson.toJson(1), serializer.toJson(1));
+        assertEquals(gson.toJson(1L), serializer.toJson(1L));
+        assertEquals(gson.toJson(1f), serializer.toJson(1f));
+        assertEquals(gson.toJson(1d), serializer.toJson(1d));
+        assertEquals(gson.toJson("aaa"), serializer.toJson("aaa"));
+        assertEquals(gson.toJson('a'), serializer.toJson('a'));
+        assertEquals(gson.toJson(new int[] {1, 2, 3}), serializer.toJson(new int[] {1, 2, 3}));
+        assertEquals(gson.toJson(List.of(1, 2 ,3)), serializer.toJson(List.of(1, 2 ,3)));
+        assertEquals(gson.toJson(Collections.singletonList(1)), serializer.toJson(Collections.singletonList(1)));
     }
 }
