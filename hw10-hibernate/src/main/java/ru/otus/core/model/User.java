81ef7e857e382abe8a7c19268f364967a00daa50
Alex <bugrimov_a@bk.ru>
2020-10-25 03:24:43 +0300

    [#19] Adding core

---
+++ hw10-hibernate/src/main/java/ru/otus/core/model/User.java
@@ -0,0 +1,41 @@
+package ru.otus.core.model;
+
+
+import javax.persistence.*;
+import java.util.StringJoiner;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    public User() {
+    }
+
+    public User(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", User.class.getSimpleName() + "[", "]")
+                .add("id=" + id)
+                .add("name='" + name + "'")
+                .toString();
+    }
+}
