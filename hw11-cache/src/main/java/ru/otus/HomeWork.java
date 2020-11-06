c3634a83b8a8782bc3909b6bfbe29985704e312c
Alex <bugrimov_a@bk.ru>
2020-11-06 19:48:54 +0300

    [#21] Refactor

--- hw11-cache/src/main/java/ru/otus/HomeWork.java
+++ hw11-cache/src/main/java/ru/otus/HomeWork.java
@@ -6,7 +6,6 @@ import ru.otus.core.dao.Dao;
 import ru.otus.core.model.AddressDataSet;
 import ru.otus.core.model.PhoneDataSet;
 import ru.otus.core.model.User;
-import ru.otus.core.service.DbService;
 import ru.otus.core.service.DbServiceWithCache;
 import ru.otus.hibernate.dao.UserDaoHibernate;
 import ru.otus.hibernate.sessionmanager.SessionManagerHibernate;
@@ -14,6 +13,8 @@ import ru.otus.hibernate.utils.HibernateUtils;
 import ru.otus.listeners.AdminListener;
 import ru.otus.listeners.Listener;
 import ru.otus.listeners.ModeratorListener;
+
+import java.util.stream.Stream;
 
 public class HomeWork {
 
@@ -26,19 +27,16 @@ public class HomeWork {
 
         final Cache<Long, User> cache = new DbCache<>();
 
-        final Listener[] listeners = {
-                new AdminListener<>(),
-                new ModeratorListener<>()
-        };
+        final var listeners = getListeners();
         cache.addListeners(listeners);
 
-        final DbService<User, Long> dbService = new DbServiceWithCache<>(userDao, cache);
+        final DbServiceWithCache<User, Long> dbService = new DbServiceWithCache<>(userDao, cache);
 
-        var userAlex = new User()
+        final User userAlex = new User()
                 .setName("Alex")
                 .setAddressDataSet(new AddressDataSet("ул. Ленина 15"))
                 .setPhones("89111231122", "89991233000", "89151230102");
-        var userIvan = new User()
+        final User userIvan = new User()
                 .setName("Ivan")
                 .setAddressDataSet(new AddressDataSet("ул. Зеленодольская 3"))
                 .setPhones("89231669192");
@@ -49,6 +47,15 @@ public class HomeWork {
         userById.ifPresent(System.out::println);
 
         cache.removeListeners(listeners);
+    }
+
+    private static Listener<Long, User>[] getListeners() {
+        final Listener<Long, User> adminListener = new AdminListener<>();
+        final Listener<Long, User> moderatorListener = new ModeratorListener<>();
+        return Stream.of(
+                adminListener,
+                moderatorListener
+        ).toArray(Listener[]::new);
     }
 
     private static SessionManagerHibernate getHibernateSessionManager() {
