a14246a35904c2340f1d0ca464231bfbe73d1ae3
Alex <bugrimov_a@bk.ru>
2020-11-10 23:36:37 +0300

    [#23] Init module

--- buildSrc/src/main/java/Versions.java
+++ buildSrc/src/main/java/Versions.java
@@ -6,5 +6,8 @@ public interface Versions {
     String asm = "8.0.1";
     String lombok = "1.18.12";
     String glassfishJson = "1.1.4";
-
+    String gson = "2.8.6";
+    String jetty = "9.4.31.v20200723";
+    String freemarker = "2.3.30";
+    String reflections = "0.9.12";
 }
